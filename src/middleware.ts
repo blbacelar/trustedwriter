aa5a74c10b1e20617eec8d36ceb285b6c97c6198
Bruno Bacelar <blbacelar@gmail.com>
2024-12-13 14:22:31 -0700

    updated clerk middleware to work with new version of nextjs 15

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/settings(.*)",
+]);
 
-export default authMiddleware({
-  publicRoutes: ["/", "sign-in", "sign-up", "/settings"],
-  afterAuth(auth, req: NextRequest) {
-    // Handle authenticated users
-    if (auth.userId && req.nextUrl.pathname === "/") {
-      const appHome = new URL("/dashboard", req.url);
-      return NextResponse.redirect(appHome);
-    }
-    return NextResponse.next();
-  },
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const appHome = new URL("/dashboard", req.url);
+    return NextResponse.redirect(appHome);
+  }
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
